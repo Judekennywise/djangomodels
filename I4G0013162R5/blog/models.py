7e8c0c3b4a568d9b73c096d2b5995deafa8751d2
Jude Oyedele <105249922+Judekennywise@users.noreply.github.com>
2022-06-20 17:35:35 +0100

    Update models.py

--- I4G0013162R5/blog/models.py
+++ I4G0013162R5/blog/models.py
@@ -7,7 +7,7 @@ from django.db.models import TextField
 
 class Post(models.Model):
     Title = models.CharField(max_length=200)
-    Text = TextField()
+    Text = model.TextField()
     Author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     Created_date = models.DateTimeField('date created')
     Published_date = models.DateTimeField('date published')
